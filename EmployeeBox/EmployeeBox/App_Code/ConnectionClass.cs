a39478ccf770d10a87b26060a1f08fe664cb0cf8
atayel <32828978+atayel@users.noreply.github.com>
2018-01-05 22:54:01 +0200

    Updates :  1- EducationQualification List and Filtration { Employee Repository }  2- Employee List and Filtration { Employee Repository }  3- View.EducationQualification  4- Add , Edit , Delete And List {Views.EducationQualification}

--- EmployeeBox/EmployeeBox/App_Code/ConnectionClass.cs
+++ EmployeeBox/EmployeeBox/App_Code/ConnectionClass.cs
@@ -2,6 +2,7 @@
 {
     internal static class ConnectionClass
     {
-        internal static string _connection = @"Data Source=197.50.99.42;Initial Catalog=EmployeesBoxDB;Persist Security Info=True;User ID=Tayel;Password=0100180;MultipleActiveResultSets=True;";
+        internal static string _connection = @"Data Source=197.50.99.42;Initial Catalog=EmployeesBoxDB;
+            Persist Security Info=True;User ID=Tayel;Password=0100180;MultipleActiveResultSets=True;";
     }
 }
